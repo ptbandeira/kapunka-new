f3a1af62f0db9b292a96b6821cfe1acd670f9131
Flagster <63302966+ptbandeira@users.noreply.github.com>
2025-09-23 11:13:38 +0200

    Allow Netlify main preview host

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,12 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+      server: {
+        allowedHosts: [
+          'devserver-preview--kapunka-new.netlify.app',
+          'devserver-main--kapunka-new.netlify.app',
+        ]
+      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
