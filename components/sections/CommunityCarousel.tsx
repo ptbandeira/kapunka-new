c1957c2a453f51d13221f15c191511d7ef836d18
Flagster <63302966+ptbandeira@users.noreply.github.com>
2025-09-30 13:16:26 +0200

    duration

--- components/sections/CommunityCarousel.tsx
+++ components/sections/CommunityCarousel.tsx
@@ -24,7 +24,7 @@ interface CommunityCarouselProps {
 }
 
 const MIN_DURATION = 4000;
-const DEFAULT_DURATION = 800;
+const DEFAULT_DURATION = 80000;
 
 const OVERLAY_POSITIONS = [
   { top: '12%', left: '12%' },
